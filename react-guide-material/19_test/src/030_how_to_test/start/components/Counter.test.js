28df5410e1dffdaf4044353d60c94ed7b6c20156
hakua <hakua1452@gmail.com>
2023-05-31 03:49:37 +0900

    テストコード
    Arrage: テストデータ、条件、環境の瀬亭
    Act: ロジックや関数の実行
    Assertion: 実行結果の確認

--- react-guide-material/19_test/src/030_how_to_test/start/components/Counter.test.js
+++ react-guide-material/19_test/src/030_how_to_test/start/components/Counter.test.js
@@ -1,6 +1,21 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+
+import Counter from "./Counter";
 /* POINT テストコードを書く際の注意点
 Arrage:(テストデータ、条件、環境の設定).
 Act:(ロジックの実行、関数の実行).
 Assertion:(実行結果と期待する結果の比較).
 */
-test('ダミーテスト:削除して記載してください。', () => {});
+test("カウントアップボタンで現在のカウントから+1されるか", () => {
+  render(<Counter originCount={0}/>);
+
+  const spanElBeforeUpdate = screen.getByText('現在のカウント:0');
+  expect(spanElBeforeUpdate).toBeInTheDocument();
+
+  // const countUpButtonEl = screen.getByText("カウントアップ");
+  const btn = screen.getByRole("button", {name: "カウントアップ"});
+  fireEvent.click(btn);
+
+  const spanEl = screen.getByText('現在のカウント:1');
+  expect(spanEl).toBeInTheDocument();
+});
