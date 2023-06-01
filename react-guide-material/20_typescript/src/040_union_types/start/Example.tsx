7a04482d5caf7177b80583da33fe5b0b45caa300
hakua <hakua1452@gmail.com>
2023-06-02 08:05:26 +0900

    typescript プリミティブ型

--- react-guide-material/20_typescript/src/040_union_types/start/Example.tsx
+++ react-guide-material/20_typescript/src/040_union_types/start/Example.tsx
@@ -1,5 +1,16 @@
 const Example = () => {
   // 練習用
+  let strOrNum: string | number = 'hello';
+  strOrNum = 123;
+  console.log(strOrNum);
+
+  let helloOrNum: 'hello' | number = 'hello';
+  helloOrNum = 456;
+  console.log(helloOrNum);
+
+  type HelloOrNum = 'hello' | number;
+  const hello: HelloOrNum = 789;
+  console.log(hello)
 };
 
 export default Example;
