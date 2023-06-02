e2b3634790d860379b07141f7a22904fdeb57f58
hakua <hakua1452@gmail.com>
2023-06-02 22:39:19 +0900

    typescript 関数の型定義

--- react-guide-material/20_typescript/src/080_function_type/start/Example.tsx
+++ react-guide-material/20_typescript/src/080_function_type/start/Example.tsx
@@ -1,5 +1,30 @@
 const Example = () => {
   // 練習用
+  // 引数の方定義
+  const sum1 = (x: number, y: number) =>{
+    return x + y;
+  }
+
+  // デフォルト値の設定
+  const sum2 = (x: number, y: number = 1) =>{
+    return x + y;
+  }
+
+  // yの引数を任意にする
+  const sum3 = (x: number, y?: number) =>{
+    console.log(y);
+    // return x + y;
+  }
+
+  // 戻り値の型定義
+  // 戻り値がない場合は :voidと定義する
+  const sum4 = (x: number, y: number): number => {
+    return x + y;
+  }
+
+  // 型エイリアス
+  type Sum = (x: number, y: number) => number;
+  const sum5: Sum = (x, y) => x + y;
 };
 
 export default Example;
