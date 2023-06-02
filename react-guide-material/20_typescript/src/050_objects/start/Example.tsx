3078b3bc5b1a2e7430f734d31db45859af08e33c
hakua <hakua1452@gmail.com>
2023-06-02 22:28:56 +0900

    typescript 型エイリアス(型に名前をつけることができる)

--- react-guide-material/20_typescript/src/050_objects/start/Example.tsx
+++ react-guide-material/20_typescript/src/050_objects/start/Example.tsx
@@ -1,5 +1,32 @@
 const Example = () => {
   // 練習用
+  // array
+  const arry1: number[] = [1,2,3];
+  const arry2: Array<number> = [1,2,3];
+
+  const arry3: (string | number)[] = [1,'a'];
+  const arry4: Array<string | number> = [1,'a'];
+
+  // object
+  const abj1:{name: string, age: number} = {
+    name: 'taro',
+    age: 18,
+  }
+  type Person = {name: string, age:number};
+  const obj2:Person = {
+    name: 'taro',
+    age: 18
+  }
+  type Person2 = {name: string, age?: number};
+  const obj3:Person2 = {
+    name: 'taro'
+  }
+
+  // array, object
+  const users: Person2[] = [
+    {name: 'taro'},
+    {name: 'taro', age: 19}
+  ]
 };
 
 export default Example;
--- react-guide-material/20_typescript/src/070_type_alias/start/Example.tsx
+++ react-guide-material/20_typescript/src/070_type_alias/start/Example.tsx
@@ -1,5 +1,18 @@
 const Example = () => {
   // 練習用
+  type User = {
+    name: string,
+    age: number,
+  }
+  const user:User = {
+    name: 'taro',
+    age: 10
+  }
+
+  type UserName = string;
+  type userAge = number;
+  type UserGender = 'man' | 'woman' | 'others';
+
 };
 
 export default Example;
