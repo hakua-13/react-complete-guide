16f6a56a7b056febd78896288d1846aa7a3929f9
hakua <hakua1452@gmail.com>
2023-06-03 14:32:38 +0900

    typescript useStateの型定義

--- react-guide-material/20_typescript/src/150_usestate/start/Example.tsx
+++ react-guide-material/20_typescript/src/150_usestate/start/Example.tsx
@@ -1,7 +1,17 @@
 import { useState } from "react";
 
+type Todo  = {
+  id: number,
+  text: string,
+}
+
 const Example = () => {
-  
+  const [ text, setText ] = useState<string>('hello');
+
+  const [ array , setArray ] = useState<number[]>([1,2,3]);
+
+  const [ todos, setTodos ] = useState<Todo[]>([{id: 0, text: "test"}])
 };
 
 export default Example;
+ 
